20f2688448d1c85ac3d6fe072c897ec29a259578
nexusLau <6267412@163.com>
2016-01-14 20:50:04 +0800

    增加logback日志

--- tintin/src/cn/com/tintin/controller/LoginController.java
+++ tintin/src/cn/com/tintin/controller/LoginController.java
@@ -1,5 +1,7 @@
 package cn.com.tintin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +9,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
+	private  Logger log = LoggerFactory.getLogger(LoginController.class);
 	
 	@RequestMapping("longIn")
 	public ModelAndView longIn(){
-		System.out.println("进入用户登录方法");
+		log.info("进入登录方式");
 		return null;
 	}
 }
