99270b71fb4167f7be5cde9bcea375d6dbf9145a
nexusLau <6267412@163.com>
2016-02-05 17:14:59 +0800

    增加对常量的管理

---
+++ tintin/src/cn/com/tintin/constant/PersonalConstant.java
@@ -0,0 +1,50 @@
+package cn.com.tintin.constant;
+
+/**|
+ * 个人
+ * 作者： liumaoyang
+ *
+ * 创建时间：2016-2-5 下午5:01:06
+ * 
+ * 实现功能：
+ */
+public class PersonalConstant {
+	
+	/**用户名*/
+	private String userName;
+	
+	/**真实姓名*/
+	private String realName;
+	
+	/**git地址*/
+	private String  gitUrl;
+	
+
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public String getGitUrl() {
+		return gitUrl;
+	}
+
+	public void setGitUrl(String gitUrl) {
+		this.gitUrl = gitUrl;
+	}
+	
+	
+	
+}
