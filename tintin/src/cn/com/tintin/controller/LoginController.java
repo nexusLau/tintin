4d85e4c733178f2434d2b71063fb925b13217926
nexusLau <6267412@163.com>
2016-02-05 23:17:39 +0800

    增加Spring对常量的管理以及调用

--- tintin/src/cn/com/tintin/controller/LoginController.java
+++ tintin/src/cn/com/tintin/controller/LoginController.java
@@ -1,5 +1,7 @@
 package cn.com.tintin.controller;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import cn.com.tintin.constant.PersonalConstant;
 import cn.com.tintin.service.user.IUserLoginInfoService;
 
 
@@ -18,10 +21,14 @@ public class LoginController {
 	@Autowired
 	private IUserLoginInfoService userLoginInfoService;
 	
+	@Resource
+	private PersonalConstant personalConstant;
+	
 	@RequestMapping("/longIn")
 	public ModelAndView longIn(String id){
 		String name=userLoginInfoService.getUserNameById(id);
 		System.out.println(name);
+		System.out.println(personalConstant.getUserName());
 		log.info("进入登录方式.12345678");
 		return null;
 	}
